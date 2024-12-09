e6390b9a046b3c3883efef1a5905b7e3f5630ffe
Ramir1914 <ramirsakatov@abv.bg>
2024-12-09 13:21:41 +0200

    Add files via upload

---
+++ Ramir  9a/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string ime="Ramir";
+            //Console.WriteLine("kak se kazvash");
+            //Console.Readline(ime);
+            Console.Beep(432, 500);
+            for (int i = 5000; i <= 50009; i += 30055)
+                Console.Beep(i, 200000000);
+             Console.Write("Hello");
+            Console.WriteLine((3522 + 52353) * 23 - (2336 * 501 + 23432 - 6743) * 3);
+        } 
+    }
+}
---
+++ Ramir  9a/ConsoleApp1/smqtane/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smqtane
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("vuvedi edno tricifreno chislo");
+            int a=int.Parse(Console.ReadLine());
+            int s, d, e;
+            s = a / 100;
+            d = s %100/10;
+            e = a %100%10;
+            Console.WriteLine($"s{s},d= {d},e= {e}");
+
+               
+            
+
+
+
+        }
+    }
+}
