df495345fb8d2e9e773edf3559216829869a83e6
Ramir1914 <ramirsakatov@abv.bg>
2025-03-20 12:06:24 +0200

    Add files via upload

---
+++ Spisuk ot imena/Spisuk ot imena/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spisuk_ot_imena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vuvedi imena: ");
+            Console.Write(" - ");
+            string[] names = Console.ReadLine().Split(' ');
+            for (int i = names.Length - 1; i >= 0; i--)
+            {
+                Console.Write(names[i]);
+                if (i > 0)
+                {
+                    Console.Write("; ");
+                }
+            }
+        }
+    }
+}
---
+++ Spisuk ot krainosti/Spisuk ot krainosti/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spisuk_ot_krainosti
+{
+    class Program
+    {
+        static void Main(string[] args)
+            //Въведете списък от цели числа и изведете тези от тях, които са равни на минималния или максималнияелемент.
+        {
+            Console.WriteLine("Vuvedi chisla:  ");
+            Console.Write(" - ");
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            int min = numbers.Min();
+            int max = numbers.Max();
+            List<int> result = new List<int>();
+            foreach (var number in numbers)
+            {
+                if (number == min || number == max)
+                {
+                    result.Add(number);
+                }
+            }
+            Console.WriteLine(string.Join(" , ", result));
+        }
+    }
+}
