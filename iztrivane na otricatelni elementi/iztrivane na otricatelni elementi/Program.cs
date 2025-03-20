5a8dd3f3121ec8ea37bda49af6ff9768b802a3e1
Ramir1914 <ramirsakatov@abv.bg>
2025-03-20 12:04:43 +0200

    Add files via upload

---
+++ iztrivane na otricatelni elementi/iztrivane na otricatelni elementi/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iztrivane_na_otricatelni_elementi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vuvedi otricatelni i polojitelni chisla: ");
+            Console.Write(" - ");
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            List<int> result = new List<int>();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] >= 0)
+                {
+                    result.Add(numbers[i]);
+                }
+            }
+            if (result.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                result.Reverse();
+                Console.WriteLine(string.Join(" , ", result));
+            }
+
+        }
+    }
+}
