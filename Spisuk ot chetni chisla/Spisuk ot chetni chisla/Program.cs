74058e4ad813fec7a83bde677d8f8f9d0bfd83a6
Ramir1914 <ramirsakatov@abv.bg>
2025-03-20 12:08:01 +0200

    Add files via upload

---
+++ Spisuk ot chetni chisla/Spisuk ot chetni chisla/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spisuk_ot_chetni_chisla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vuvedi chisla: ");
+            Console.Write(" - ");
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            List<int> evenNumbers = new List<int>();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    evenNumbers.Add(numbers[i]);
+                }
+            }
+            Console.WriteLine(string.Join(" ", evenNumbers));
+        }
+    }
+}
